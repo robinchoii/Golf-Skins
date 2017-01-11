99bc7227302b4b33106f77423779db8026e346f1
robinchoii <robinchoii@gmail.com>
2017-01-11 15:32:04 -0800

    calculate total skins pars birdies kps other

--- javascript/main.js
+++ javascript/main.js
@@ -1,51 +1,62 @@
 var app = angular.module('myApp', [] );
 
 app.controller('mainController',function($scope) {
-  // $scope.player1 = $scope.player1;
-  // $scope.player2 = $scope.player2;
-  // $scope.player3 = $scope.player3;
-  // $scope.player4 = $scope.player4;
-
-  // $scope.player1 = {
-  //   name: $scope.player1Name,
-  //   skins: $scope.player1Skins,
-  //   pars: $scope.player1Pars,
-  //   birdies: $scope.player1Birdies,
-  //   kps: $scope.player1KPs,
-  //   other: $scope.player1Other
-  // };
-  // $scope.skins = skins
-
+  $scope.settings = {
+    skin: 0,
+    par: 0,
+    birdie: 0,
+    kp: 0,
+    other: 0
+  };
   $scope.player1 = {
-    name: $scope.player1Name,
-    skins: $scope.player1Skins,
-    pars: $scope.player1Pars,
-    birdies: $scope.player1Birdies,
-    kps: $scope.player1KPs,
-    other: $scope.player1Other
-  },
+    name: '',
+    skins: 0,
+    pars: 0,
+    birdies: 0,
+    kps: 0,
+    other: 0
+  };
   $scope.player2 = {
-    name: $scope.player2Name,
-    skins: $scope.player2Skins,
-    pars: $scope.player2Pars,
-    birdies: $scope.player2Birdies,
-    kps: $scope.player2KPs,
-    other: $scope.player2Other
-  },
+    name: '',
+    skins: 0,
+    pars: 0,
+    birdies: 0,
+    kps: 0,
+    other: 0
+  };
   $scope.player3 = {
-    name: $scope.player3Name,
-    skins: $scope.player3Skins,
-    pars: $scope.player3Pars,
-    birdies: $scope.player3Birdies,
-    kps: $scope.player3KPs,
-    other: $scope.player3Other
-  },
+    name: '',
+    skins: 0,
+    pars: 0,
+    birdies: 0,
+    kps: 0,
+    other: 0
+  };
   $scope.player4 = {
-    name: $scope.player4Name,
-    skins: $scope.player4Skins,
-    pars: $scope.player4Pars,
-    birdies: $scope.player4Birdies,
-    kps: $scope.player4KPs,
-    other: $scope.player4Other
-  }
+    name: '',
+    skins: 0,
+    pars: 0,
+    birdies: 0,
+    kps: 0,
+    other: 0
+  };
+
+  $scope.generate = function() {
+    var skinValue = $scope.settings.skin;
+    var parValue = $scope.settings.par;
+    var birdieValue = $scope.settings.birdie;
+    var kpValue = $scope.settings.kp;
+    var otherValue = $scope.settings.other;
+
+    var player1Total = $scope.player1.skins * skinValue + $scope.player1.pars * parValue + $scope.player1.birdies * birdieValue + $scope.player1.kps * kpValue + $scope.player1.other * otherValue
+    var player2Total = $scope.player2.skins * skinValue + $scope.player2.pars * parValue + $scope.player2.birdies * birdieValue + $scope.player2.kps * kpValue + $scope.player2.other * otherValue
+    var player3Total = $scope.player3.skins * skinValue + $scope.player3.pars * parValue + $scope.player3.birdies * birdieValue + $scope.player3.kps * kpValue + $scope.player3.other * otherValue
+    var player4Total = $scope.player4.skins * skinValue + $scope.player4.pars * parValue + $scope.player4.birdies * birdieValue + $scope.player4.kps * kpValue + $scope.player4.other * otherValue
+
+    console.log(player1Total)
+    console.log(player2Total)
+    console.log(player3Total)
+    console.log(player4Total)
+    console.log($scope.player1)
+  };
 })
